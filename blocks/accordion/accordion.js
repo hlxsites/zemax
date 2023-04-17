421a43c322406782646856128a9500be7c397fcf
Kunwar <kunwarsaluja@hotmail.com>
2023-04-17 16:19:55 -0400

    adding slidedown transition

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -8,6 +8,12 @@ export default function decorate(block) {
     faqQuestion.addEventListener('click', (e) => {
       e.currentTarget.classList.toggle('active');
       e.currentTarget.nextElementSibling.classList.toggle('active');
+      const faqAnswer = e.currentTarget.nextElementSibling;
+      if (faqAnswer.style.maxHeight) {
+        faqAnswer.style.maxHeight = null;
+      } else {
+        faqAnswer.style.maxHeight = `${faqAnswer.scrollHeight}px`;
+      }
     });
 
     const faqAnswer = [...row.children][1];
