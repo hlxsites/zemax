ddbc1d5dc0ada7a0cc81e96d6dc6b9b585883126
Kunwar <kunwarsaluja@hotmail.com>
2023-04-17 17:36:14 -0400

    updates per feedback from review

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -6,11 +6,15 @@ export default function decorate(block) {
     const faqQuestion = [...row.children][0];
     faqQuestion.classList.add('faq-question');
     faqQuestion.addEventListener('click', (e) => {
+      const openfaq = block.querySelector('.faq-question.active');
+      openfaq.classList.remove('active');
+      openfaq.nextElementSibling.classList.remove('active');
+      openfaq.nextElementSibling.style.removeProperty('max-height');
       e.currentTarget.classList.toggle('active');
       e.currentTarget.nextElementSibling.classList.toggle('active');
       const faqAnswer = e.currentTarget.nextElementSibling;
       if (faqAnswer.style.maxHeight) {
-        faqAnswer.style.maxHeight = null;
+        faqAnswer.style.removeProperty('max-height');
       } else {
         faqAnswer.style.maxHeight = `${faqAnswer.scrollHeight}px`;
       }
@@ -19,4 +23,10 @@ export default function decorate(block) {
     const faqAnswer = [...row.children][1];
     faqAnswer.classList.add('faq-answer');
   });
+
+  const fq = block.querySelector('.faq-question');
+  const fa = block.querySelector('.faq-answer');
+  fq.classList.toggle('active');
+  fa.classList.toggle('active');
+  fa.style.maxHeight = 'unset';
 }
