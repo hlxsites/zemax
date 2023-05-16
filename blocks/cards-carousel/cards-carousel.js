a03e84dae76fd9f045c02bd68612e8ede9a3f803
zjun <zjun@adobe.com>
2023-05-16 16:21:15 -0700

    refine carousel

--- blocks/cards-carousel/cards-carousel.js
+++ blocks/cards-carousel/cards-carousel.js
@@ -2,17 +2,18 @@ const SLIDE_ID_PREFIX = 'cards-carousel-slide';
 const SLIDE_CONTROL_ID_PREFIX = 'cards-carousel-slide-control';
 
 let curSlide = 0;
+let maxSlide = 0;
 let slideShow = 3;
 
 /**
  * Keep active dot in sync with current slide
- * @param carousel The carousel
- * @param activeSlide {number} The active slide
+ * @param carouselWrapper The carousel
+ * @param activeDotIndex {number} The active dot
  */
-function syncActiveDot(carousel, activeSlide) {
-  carousel.querySelectorAll('ul.carousel-dots li').forEach((item, index) => {
+function syncActiveDot(carouselWrapper, activeDotIndex) {
+  carouselWrapper.querySelectorAll('ul.carousel-dots li').forEach((item, index) => {
     const btn = item.querySelector('button');
-    if (index === activeSlide) {
+    if (index === activeDotIndex) {
       item.classList.add('carousel-dots-active');
       btn.setAttribute('aria-selected', 'true');
       btn.setAttribute('tabindex', '0');
@@ -27,14 +28,14 @@ function syncActiveDot(carousel, activeSlide) {
 /**
  * Scroll a single slide into view.
  *
- * @param carousel The carousel
+ * @param carouselWrapper The carousel
  * @param slideIndex {number} The slide index
  */
-function scrollToSlide(carousel, slideIndex) {
-  const carouselSlider = carousel.querySelector('.cards-carousel-slide-container');
+function scrollToSlide(carouselWrapper, slideIndex) {
+  const carouselSlider = carouselWrapper.querySelector('.cards-carousel-slide-container');
   const leftPos = (carouselSlider.clientWidth / slideShow) * slideIndex;
   carouselSlider.scrollTo({ left: leftPos, behavior: 'smooth' });
-  syncActiveDot(carousel, slideIndex);
+  syncActiveDot(carouselWrapper, slideIndex / slideShow);
   // sync slide
   [...carouselSlider.children].forEach((slide, index) => {
     if (index === slideIndex) {
@@ -49,39 +50,39 @@ function scrollToSlide(carousel, slideIndex) {
 }
 
 /**
- *
- * @param slides An array of slide elements within the carousel
- * @return {HTMLUListElement} The carousel dots element
+ * Build dots for controlling the carousel
+ * @param {*} dotNum the number of dots to build
+ * @returns {HTMLUListElement} The carousel dots element
  */
-function buildDots(size) {
+function buildDots(dotNum) {
   const dots = document.createElement('ul');
   dots.classList.add('carousel-dots');
   dots.setAttribute('role', 'tablist');
-  [...Array(size)].forEach((_, index) => {
+  [...Array(dotNum)].forEach((_, dotIndex) => {
     const dotItem = document.createElement('li');
     dotItem.setAttribute('role', 'presentation');
-    if (index === 0) {
+    if (dotIndex === 0) {
       dotItem.classList.add('carousel-dots-active');
     }
     const dotBtn = document.createElement('button');
-    dotBtn.setAttribute('id', `${SLIDE_CONTROL_ID_PREFIX}${index}`);
+    dotBtn.setAttribute('id', `${SLIDE_CONTROL_ID_PREFIX}${dotIndex}`);
     dotBtn.setAttribute('type', 'button');
     dotBtn.setAttribute('role', 'tab');
-    dotBtn.setAttribute('aria-controls', `${SLIDE_ID_PREFIX}${index}`);
-    if (index === 0) {
+    dotBtn.setAttribute('aria-controls', `${SLIDE_ID_PREFIX}${dotIndex}`);
+    if (dotIndex === 0) {
       dotBtn.setAttribute('aria-selected', 'true');
       dotBtn.setAttribute('tabindex', '0');
     } else {
       dotBtn.setAttribute('tabindex', '-1');
     }
-    dotBtn.setAttribute('aria-label', `${index + 1} of ${size}`);
+    dotBtn.setAttribute('aria-label', `${dotIndex + 1} of ${dotNum}`);
 
-    dotBtn.innerText = `${index + 1}`;
+    dotBtn.innerText = `${dotIndex + 1}`;
     dotItem.append(dotBtn);
     dotItem.addEventListener('click', (e) => {
-      curSlide = index;
-      const carousel = e.target.closest('.cards-carousel');
-      scrollToSlide(carousel, curSlide);
+      curSlide = dotIndex * slideShow;
+      const carouselWrapper = e.target.closest('.cards-carousel-wrapper');
+      scrollToSlide(carouselWrapper, curSlide);
     });
     dots.append(dotItem);
   });
@@ -90,11 +91,43 @@ function buildDots(size) {
 }
 
 /**
+ * Build a navigation button for controlling the direction of logos slides.
+ *
+ * @param dir A string of either 'prev or 'next'
+ * @return {HTMLDivElement} The resulting nav element
+ */
+function buildNav(dir) {
+  const nav = document.createElement('div');
+  nav.classList.add('carousel-nav', `carousel-nav-${dir}`);
+  nav.addEventListener('click', (e) => {
+    let nextSlide = 0;
+    if (dir === 'prev') {
+      if (curSlide <= 0) {
+        nextSlide = maxSlide - slideShow + 1;
+      } else {
+        nextSlide = curSlide - slideShow;
+      }
+      if (nextSlide < 0) {
+        nextSlide = 0;
+      }
+    } else {
+      nextSlide = curSlide + slideShow;
+      if (nextSlide > maxSlide) {
+        nextSlide = 0;
+      }
+    }
+    const carouselWrapper = e.target.closest('.cards-carousel-wrapper');
+    scrollToSlide(carouselWrapper, nextSlide);
+  });
+  return nav;
+}
+
+/**
  * Decorate a base slide element.
  *
  * @param slide A base block slide element
  * @param index The slide's position
- * @return {HTMLUListElement} A decorated carousel slide element
+ * @return {HTMElement} A decorated carousel slide element
  */
 function buildSlide(slide, index) {
   [...slide.children].forEach((div) => {
@@ -142,8 +175,8 @@ function snapScroll(el, dir = 1) {
   const block = Math.floor(el.scrollLeft / el.offsetWidth);
   const pos = el.scrollLeft - el.offsetWidth * block;
   const snapToBlock = pos <= threshold ? block : block + 1;
-  const carousel = el.closest('.cards-carousel');
-  scrollToSlide(carousel, snapToBlock);
+  const carouselWrapper = el.closest('.cards-carousel-wrapper');
+  scrollToSlide(carouselWrapper, snapToBlock);
 }
 
 /**
@@ -189,10 +222,10 @@ function makeCarouselDraggable(carousel) {
   });
 }
 
-function calculateSlideShow() {
+function calculateSlideShow(defaultVariant) {
   const totalWidth = window.innerWidth;
   if (totalWidth >= 992) {
-    slideShow = 3;
+    slideShow = defaultVariant ? 1 : 3;
   } else if (totalWidth >= 767) {
     slideShow = 2;
   } else {
@@ -205,30 +238,36 @@ function addDotsIfNeed(slideLength) {
   // remove old dots if there is, add dots if there is need
   document.querySelector('.carousel-dots')?.remove();
   if (dotsNum > 1) {
-    document.querySelector('.block.cards-carousel').append(buildDots(dotsNum));
+    document.querySelector('.cards-carousel-wrapper').append(buildDots(dotsNum));
     // while dots added, scroll to first slide
     const carouselSlider = document.querySelector('.cards-carousel-slide-container');
     carouselSlider.scrollTo({ left: 0, behavior: 'smooth' });
   }
 }
 
-function responsiveHandler(block, slideLength) {
+function responsiveHandler(slideLength, defaultVariant) {
   // initial add dots if need
-  calculateSlideShow();
+  calculateSlideShow(defaultVariant);
   addDotsIfNeed(slideLength);
   window.matchMedia('(min-width: 992px)').addEventListener('change', (mediaQuery) => {
     if (mediaQuery.matches) {
-      slideShow = 3;
+      slideShow = defaultVariant ? 1 : 3;
       addDotsIfNeed(slideLength);
     }
   });
   window.matchMedia('(min-width: 767px) and (max-width: 992px)').addEventListener('change', (mediaQuery) => {
     if (mediaQuery.matches) {
-      slideShow = 2;
+      slideShow = defaultVariant ? 1 : 2;
       addDotsIfNeed(slideLength);
     }
   });
-  window.matchMedia('(max-width: 767px)').addEventListener('change', (mediaQuery) => {
+  window.matchMedia('(min-width: 640px) and (max-width: 767px)').addEventListener('change', (mediaQuery) => {
+    if (mediaQuery.matches) {
+      slideShow = defaultVariant ? 2 : 1;
+      addDotsIfNeed(slideLength);
+    }
+  });
+  window.matchMedia('(max-width: 640px)').addEventListener('change', (mediaQuery) => {
     if (mediaQuery.matches) {
       slideShow = 1;
       addDotsIfNeed(slideLength);
@@ -249,6 +288,7 @@ export default function decorate(block) {
 
   // build carousel slides
   const slides = [...block.children];
+  maxSlide = slides.length - 1;
   slides.forEach((slide, index) => {
     carousel.appendChild(buildSlide(slide, index));
   });
@@ -256,5 +296,14 @@ export default function decorate(block) {
   // add decorated carousel to block
   block.append(carousel);
 
-  responsiveHandler(block, slides.length);
+  // add nav buttons if that is a feature variant
+  const featureVariant = block.classList.contains('feature');
+  if (featureVariant) {
+    const prevBtn = buildNav('prev');
+    block.prepend(prevBtn);
+    const nextBtn = buildNav('next');
+    block.append(nextBtn);
+  }
+
+  responsiveHandler(slides.length, featureVariant);
 }
