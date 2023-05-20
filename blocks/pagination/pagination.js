005369363b6c32035a2d5fbf69250a51936f9c34
Aravindo Wingeier <wingeier@adobe.com>
2023-05-19 17:54:39 -0700

    simplify pagination

--- blocks/pagination/pagination.js
+++ blocks/pagination/pagination.js
@@ -7,88 +7,85 @@ function getUrlForPage(pageNo) {
   return `?${searchParams}`;
 }
 
-function addLink(list, str, page) {
-  const li = document.createElement('li');
-  if (page != null) {
-    li.append(a({ href: getUrlForPage(page) }, str));
+function createPageItems(currentPage, totalPages) {
+  const maxPages = 5;
+  let startPage;
+  let endPage;
+
+  if (totalPages <= maxPages) {
+    // If there are 5 pages or less, show all pages
+    startPage = 1;
+    endPage = totalPages;
   } else {
-    li.append(str);
+    // If there are more than 5 pages
+    if (currentPage <= Math.floor(maxPages / 2)) {
+      // If the current page is at the start
+      startPage = 1;
+      endPage = maxPages;
+    } else if (currentPage + Math.floor(maxPages / 2) >= totalPages) {
+      // If the current page is at the end
+      startPage = totalPages - maxPages + 1;
+      endPage = totalPages;
+    } else {
+      // Anywhere in the middle
+      startPage = currentPage - Math.floor(maxPages / 2);
+      endPage = startPage + maxPages - 1;
+    }
   }
-  list.appendChild(li);
+
+  // Create an array of page numbers
+  const pages = [];
+  for (let i = startPage; i <= endPage; i++) {
+    pages.push(i);
+  }
+  return pages;
+}
+
+function createPagination(block, page, pages) {
+  const list = ul();
+
+  const pageItems = createPageItems(page, pages);
+
+  function addLink(ignore, str, ignore2) {
+    const li = document.createElement('li');
+    if (str !== '...') {
+      li.append(a({ href: getUrlForPage(page), class: Number(str) === page ? 'current-page' : '' }, str));
+    } else {
+      li.append(str);
+    }
+    list.appendChild(li);
+  }
+
+  // prev
+  addLink(list, ' < ', 1);
+
+  // always show first page
+  if (pageItems[0] !== 1) {
+    addLink(list, 1, null);
+    addLink(list, '...', null);
+  }
+
+  // list pages around current page
+  pageItems.forEach((pageItem) => {
+    addLink(list, pageItem, pageItem);
+  });
+
+  // always show last page
+  if (pageItems[pageItems.length - 1] !== pages) {
+    addLink(list, '...', null);
+    addLink(list, pages, null);
+  }
+
+  // next
+  addLink(list, ' > ', page + 1);
+
+  return list;
 }
 
 export default async function decorate(block) {
   const { page, pages } = readBlockConfig(block);
 
-  const pageCountLast = pages - 4;
-  const lastIter = pages - page;
-  const itemFirst = page - 2;
-  const itemSecond = page - 1;
-  const itemLast = page + 1;
-
-  const list = ul();
-  block.append(list);
-
-  // algorithm for pagination copied from https://www.zemax.com/search?view=knowledgebase&q=OpticStudio
-  // could be replaced with something more readable.
-  if (page > 1) {
-    addLink(list, ' < ', 1);
-  } else {
-    addLink(list, '<', 1);
-  }
-
-  // eslint-disable-next-line no-plusplus
-  for (let i = 1; i <= pages; i++) {
-    if (pages <= 5) {
-      if (page === i) {
-        addLink(list, i, i);
-      } else if (i <= pages) {
-        addLink(list, i, i);
-      }
-    } else if (page < 5) {
-      if (page === i) {
-        addLink(list, i, i);
-      } else if (i === 6) {
-        addLink(list, '...', null);
-      } else if (i === 7) {
-        addLink(list, pages, pages);
-      } else {
-        addLink(list, i, i);
-      }
-    } else if (page >= 5 && page <= pageCountLast) {
-      if (i === 1) {
-        addLink(list, i, i);
-        addLink(list, '...', null);
-      } else if (i === 4) {
-        addLink(list, page, page);
-      } else if (i === 6) {
-        addLink(list, '...');
-        addLink(list, pages, pages);
-      } else if (i === 2) {
-        addLink(list, itemFirst, itemFirst);
-      } else if (i === 3) {
-        addLink(list, itemSecond, itemSecond);
-      } else if (i === 5) {
-        addLink(list, itemLast, itemLast);
-      }
-    } else if (page >= pageCountLast) {
-      if (i === 1) {
-        addLink(list, 1, 1);
-      } else if (7 - lastIter === i) {
-        addLink(list, page, page);
-      } else if (i === 2) {
-        addLink(list, '...', null);
-      } else if (i === 7) {
-        addLink(list, pages, pages);
-      } else {
-        addLink(list, pages - 7 + i, pages - 7 + i);
-      }
-    }
-  }
-
-  if (page < pages) {
-    addLink(list, ' > ', page + 1);
-  } else {
-    addLink(list, '&gt;', null);
+  for (let i = 1; i <= 20; i++) {
+    block.append(createPagination(block, i, 20));
   }
 }
