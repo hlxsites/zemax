fe811e28023d2e5a232f28356cbfe7729cf9eb5c
Aravindo Wingeier <wingeier@adobe.com>
2023-05-19 17:08:59 -0700

    refactor

--- blocks/pagination/pagination.js
+++ blocks/pagination/pagination.js
@@ -1,15 +1,21 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
-import { ul } from '../../scripts/dom-helpers.js';
+import { a, ul } from '../../scripts/dom-helpers.js';
 
-function addLink(list, s) {
+function addLink(list, str, page) {
   const li = document.createElement('li');
-  li.innerHTML = s;
+  if (page != null) {
+    li.append(a({ href: `?page=${page}` }, str));
+  } else {
+    li.append(str);
+  }
   list.appendChild(li);
 }
 
 export default async function decorate(block) {
-  const { page, pages } = readBlockConfig(block);
+  let { page, pages } = readBlockConfig(block);
 
+  page = 7;
+  pages = 34;
   const pageCountLast = pages - 4;
   const lastIter = pages - page;
   const itemFirst = page - 2;
@@ -20,63 +26,63 @@ export default async function decorate(block) {
   block.append(list);
 
   if (page > 1) {
-    addLink(list, '<li><a href=""> &lt; </a></li>');
+    addLink(list, ' < ', 1);
   } else {
-    addLink(list, '<li><span> &lt; </span></li>');
+    addLink(list, '<', 1);
   }
 
   // eslint-disable-next-line no-plusplus
   for (let i = 1; i <= pages; i++) {
     if (pages <= 5) {
       if (page === i) {
-        addLink(list, `<li><a href="" class="current-page">${i}</a></li>`);
+        addLink(list, i, i);
       } else if (i <= pages) {
-        addLink(list, `<li><a href="">${i}</a></li>`);
+        addLink(list, i, i);
       }
     } else if (page < 5) {
       if (page === i) {
-        addLink(list, `<li><a href="" class="current-page">${i}</a></li>`);
+        addLink(list, i, i);
       } else if (i === 6) {
-        addLink(list, '<li><span class="dotted-page">...</span></li>');
+        addLink(list, '...', null);
       } else if (i === 7) {
-        addLink(list, `<li><a href="">${pages}</a></li>`);
+        addLink(list, pages, pages);
       } else {
-        addLink(list, `<li><a href="">${i}</a></li>`);
+        addLink(list, i, i);
       }
     } else if (page >= 5 && page <= pageCountLast) {
       if (i === 1) {
-        addLink(list, `<li><a href="">${i}</a></li>`);
-        addLink(list, '<li><a href="" class="dotted-page">...</a></li>');
+        addLink(list, i, i);
+        addLink(list, '...', null);
       } else if (i === 4) {
-        addLink(list, `<li><a href="" class="current-page">${page}</a></li>`);
+        addLink(list, page, page);
       } else if (i === 6) {
-        addLink(list, '<li><span class="dotted-page">...</span></li>');
-        addLink(list, `<li><a href="">${pages}</a></li>`);
+        addLink(list, '...');
+        addLink(list, pages, pages);
       } else if (i === 2) {
-        addLink(list, `<li><a href="">${itemFirst}</a></li>`);
+        addLink(list, itemFirst, itemFirst);
       } else if (i === 3) {
-        addLink(list, `<li><a href="">${itemSecond}</a></li>`);
+        addLink(list, itemSecond, itemSecond);
       } else if (i === 5) {
-        addLink(list, `<li><a href="">${itemLast}</a></li>`);
+        addLink(list, itemLast, itemLast);
       }
     } else if (page >= pageCountLast) {
       if (i === 1) {
-        addLink(list, '<li><a href="" >1</a></li>');
+        addLink(list, 1, 1);
       } else if (7 - lastIter === i) {
-        addLink(list, `<li><a href="" class="current-page">${page}</a></li>`);
+        addLink(list, page, page);
       } else if (i === 2) {
-        addLink(list, '<li><span class="dotted-page">...</span></li>');
+        addLink(list, '...', null);
       } else if (i === 7) {
-        addLink(list, `<li><a href="">${pages}</a></li>`);
+        addLink(list, pages, pages);
       } else {
-        addLink(list, `<li><a href="">${pages - 7 + i}</a></li>`);
+        addLink(list, pages - 7 + i, pages - 7 + i);
       }
     }
   }
 
   if (page < pages) {
-    addLink(list, '<li><a href=""> &gt; </a></li>');
+    addLink(list, ' > ', page + 1);
   } else {
-    addLink(list, '<li><span> &gt; </span></li>');
+    addLink(list, '<span> &gt; </span>');
   }
 }
