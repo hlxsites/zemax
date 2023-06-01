ad4551d4e9267e2b34efedb40735fd5504c6ee4d
ANSYS\shkhan <shehjad.khan@ansys.com>
2023-06-01 00:47:03 -0500

    issue-174: Code refactoring

---
+++ configs/forms/addColleagueFormConfig.js
@@ -0,0 +1,17 @@
+const addColleagueFormConfiguration = {
+  fields: [
+    { id: 'first-name', label: 'First Name' },
+    { id: 'last-name', label: 'Last Name' },
+    { id: 'job-title', label: 'Job Title' },
+    { id: 'email', label: 'Email' },
+    { id: 'phone', label: 'Business Phone' },
+    { id: 'personal-phone', label: 'Mobile Phone' },
+  ],
+  submitText: 'Add Colleague',
+  submitId: 'addColleagueSubmitButton',
+  submitClass: 'add-colleague-action-button primary button',
+  submitDataModalId: 'addColleagueModal',
+  formId: 'addColleagueEditForm',
+};
+
+export default addColleagueFormConfiguration;
---
+++ configs/forms/editUserFormConfig.js
@@ -0,0 +1,22 @@
+const editUserFormConfiguration = {
+  fields: [
+    {
+      id: 'first-name', label: 'First Name', value: '', readOnly: true, disabled: true,
+    },
+    {
+      id: 'last-name', label: 'Last Name', value: '', readOnly: true, disabled: true,
+    },
+    { id: 'job-title', label: 'Job Title' },
+    {
+      id: 'email', label: 'Email', value: '', readOnly: true, disabled: true,
+    },
+    { id: 'phone', label: 'Business Phone' },
+  ],
+  submitText: 'Submit',
+  submitId: 'userEditSubmitButton',
+  submitClass: 'edit-user-action-button',
+  submitDataModalId: 'editUserModal',
+  formId: 'editUserEditForm',
+};
+
+export default editUserFormConfiguration;
