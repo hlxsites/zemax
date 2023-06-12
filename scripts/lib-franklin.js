6d1d70fba94b4606a984d8ae8e4a4a4b288d80fe
Kiran Murugulla <murugull@adobe.com>
2023-06-12 13:37:56 -0400

    avoiding undefined height, width values

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -479,8 +479,8 @@ export function createOptimizedPicture(src, alt = '', eager = false, breakpoints
       img.setAttribute('alt', alt);
       picture.appendChild(img);
       img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
-      img.setAttribute('width', `${br.width}`);
-      img.setAttribute('height', `${br.height}`);
+      if (br.width) img.setAttribute('width', `${br.width}`);
+      if (br.height) img.setAttribute('height', `${br.height}`);
     }
   });
 
