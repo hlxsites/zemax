50d32c407703916fc2fc4b137887009abc697ed4
ANSYS\shkhan <shehjad.khan@ansys.com>
2023-07-11 11:02:21 -0500

    issue-234: Adding google tag manager

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,7 @@ import { sampleRUM, fetchPlaceholders } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
+loadGoogleTagManager();
 // fetch placeholders file
 const placeholders = await fetchPlaceholders();
 
@@ -32,4 +32,13 @@ if (!window.location.host.includes('hlx.page') && !window.location.host.includes
       });
     }
   });
+}
+
+async function loadGoogleTagManager() {
+  // google tag manager
+  // eslint-disable-next-line func-names
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); const f = d.getElementsByTagName(s)[0]; const j = d.createElement(s); const
+      dl = l !== 'dataLayer' ? `&l=${l}` : ''; j.async = true; j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`; f.parentNode.insertBefore(j, f);
+  }(window, document, 'script', 'dataLayer', 'GTM-PXZBLWC'));
 }
